dfc178d8444031e03d36cfcd6349ed6e4a054a7d
BasilNozdrin <bassvass@gmail.com>
2022-06-03 09:07:09 +0300

    [DE] dz

--- 3rd course/2nd semester/DifferentialEquations/DZ1/dz1.m
+++
@@ -1,7 +0,0 @@
-a = 0; b = 2; ya = 1; yb = 2;
-f = @(x,y) log(x+1)*pow2(y);
-
-lsp = linspace(a,b,50);
-[x,y] = ode45(f, [a,b], [ya,yb]);
-plot(x,y);
-# print -djpg figure-lsnode.jpg
---
+++ 3rd course/2nd semester/DifferentialEquations/DZ1/f.m
@@ -0,0 +1,3 @@
+function dydx = f(x,y)
+    dydx = [y(2); log(x+1)*y*y];
+end
